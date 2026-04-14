fbe160f6f57a19e4028f485b496610feee348d74
MavludaQA <m.khoshimbaeva@team.alif.tj>
2026-04-14 14:35:51 +0500

    creat progect

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+
+@pytest.fixture
+def page():
+    with sync_playwright() as drv:
+        browser = drv.chromium.launch(headless=False)
+        page_ = browser.new_page()
+        page_.set_default_timeout(7000)
+        yield page_
+        browser.close()
